b50a7b72971d6084c9b4d16e9b871671a356ed39
tbsdvl <tristonburns97@gmail.com>
2021-03-28 21:49:16 -0700

    Database pseudo and testing SQL code

---
+++ db/database.sql
@@ -0,0 +1,30 @@
+CREATE DATABASE employee_tracker;
+
+USE employee_tracker;
+
+CREATE TABLE department(
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role(
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+    FOREIGN KEY (department_id),
+        REFERENCES department(id)
+);
+
+
+CREATE TABLE employee(
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT,
+    FOREIGN KEY (role_id)
+        REFERENCES role(id)
+);
+
+SHOW TABLES;
