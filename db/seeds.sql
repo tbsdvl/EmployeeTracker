6fbc7ec77c40b3da6c56a70dfd01819ab1a14dd1
tbsdvl <tristonburns97@gmail.com>
2021-03-30 19:20:16 -0700

    Adding department, role, employee insertion logic

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,7 +1,7 @@
 USE employee_trackerDB;
 
 INSERT INTO department (name)
-VALUES ("Human Resources"), ("Maintenance"), ("Logistics"), ("Medicine"), ("Local Wildlife Removal"), ("Sanitation");
+VALUES ("Human Resources"), ("Maintenance"), ("Logistics"), ("Medicine"), ("Animal Control"), ("Sanitation");
 
 INSERT INTO role (title, salary, department_id)
 VALUES ("Vicar", 60000.0, 1), ("Engineer", 75000.0, 2), ("Stevedore", 45000.0, 3), ("Medic", 80000.0, 4), ("Merc", 55000.0, 5), ("Sanitation Auto Mechanical", 0.0, 6);
